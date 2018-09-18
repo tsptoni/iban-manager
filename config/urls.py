1f398a110c053894a7ea0c6fdb5f332cb9d53b37
Antonio Sanchez Pineda <tsptoni@gmail.com>
2018-09-18 23:00:01 +0200

    Ready to start development using this project base.

--- config/urls.py
+++ config/urls.py
@@ -1,9 +1,7 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
-from django.conf import settings
 from django.conf.urls import include, url
-from django.conf.urls.static import static
 from django.contrib import admin
 
 urlpatterns = [
@@ -12,5 +10,5 @@ urlpatterns = [
         url(r'^api/v1/users/', include('ibanmanager.users.urls_api', namespace='api-v1-users')),
         url(r'^api/v1/bank/', include('ibanmanager.bank.urls_api', namespace='api-v1-bank')),
 
-] #+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+]
 
--- ibanmanager/bank/models.py
+++ ibanmanager/bank/models.py
@@ -2,6 +2,8 @@
 
 from __future__ import unicode_literals, absolute_import
 
+from django.db import models
 from ibanmanager.utils.models.base import BaseModel
+
 
 
