0fdc89ff5a4332a8f36438488638736c52b456b6
Antonio Sanchez Pineda <tsptoni@gmail.com>
2018-09-20 12:34:26 +0200

    IBAN-11 #comment IBAN-11 Checked and working permissions for CRUD operations for individual users.

--- ibanmanager/users/views_api.py
+++ ibanmanager/users/views_api.py
@@ -1,11 +1,9 @@
 # -*- coding: utf-8 -*-
 
-from rest_framework.viewsets import GenericViewSet
-from rest_framework import mixins as rf_mixins
+from rest_framework.viewsets import ModelViewSet
 from rest_framework import permissions
 from ibanmanager.users import serializers as user_serializers
 from ibanmanager.users import models as user_models
-from ibanmanager.utils.rest.mixins import PartialUpdateModelMixin
 
 
 class UserCustomPermission(permissions.IsAuthenticated):
@@ -17,16 +15,13 @@ class UserCustomPermission(permissions.IsAuthenticated):
         if request.method in permissions.SAFE_METHODS:
             return True
         else:
-            return obj.created_by.pk == request.user.pk
+            return obj.created_by == request.user
 
 
-class UserViewSet(rf_mixins.RetrieveModelMixin,
-                  PartialUpdateModelMixin,
-                  rf_mixins.ListModelMixin,
-                  GenericViewSet):
+class UserViewSet(ModelViewSet):
 
     serializer_class = user_serializers.UserSerializer
-    filter_fields = ('type', 'is_superuser')
+    filter_fields = ('type',)
 
     queryset = user_models.User.objects.all()
     permission_classes = (UserCustomPermission,)
@@ -34,15 +29,10 @@ class UserViewSet(rf_mixins.RetrieveModelMixin,
     def get_queryset(self):
         result = user_models.User.objects.filter(type=user_models.USER_TYPE.INDIVIDUAL)
 
-        if self.request.query_params.get('is_superuser', None) != None:
-            filter_is_superuser = self.request.query_params.get('is_superuser', 'false').lower() == 'true'
-            result = result.filter(is_superuser=filter_is_superuser)
-
         return user_serializers.UserSerializer.setup_eager_loading(result)
 
 
     def get_serializer_context(self):
-        context = {
-        }
+        context = {}
         return context
 
